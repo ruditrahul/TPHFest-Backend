99692577600909322cc15a7c1bfbf16090e6303f
Ayanabha Misra <misrarimbo@gmail.com>
2021-11-20 09:37:40 +0530

    Payment endpoint added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
+const razorpay = require("razorpay");
+
 const cors = require("cors");
 const app = express();
 
@@ -13,6 +15,11 @@ app.use(bodyParser.json());
 
 app.use("/api/v1/user", userRoutes);
 
+var instance = new razorpay({
+  key_id: "rzp_test_DC9Y7TPOChYcuP",
+  key_secret: "RvdcKmFgkplVjxWqmMldMYCV",
+});
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -22,6 +29,10 @@ mongoose
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 8000;
+
+app.get("/payment", (req, res) => {
+  res.status(200).json("Hello ji");
+});
 
 app.listen(PORT, () => {
   console.log(`Server ${PORT} is up and running`);
